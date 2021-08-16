180f90117a57450ff3e49788d2517f43d824b96f
ThreatMatrix <65027827+ThreatMatrix@users.noreply.github.com>
2021-08-16 20:32:36 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
-var adjectives = ["Terrible", "Haunted", "Forgotten","Stupified", "Abandoned", "Tortured", "Inclement", "Apologetic", "Riddled", "Unfortunate", "Neglected", "Forgotten",
- "Sordid", "Mistaken", "Glutonous", "Bedraggled", "Stricken", "Confused", "Beaten", "Sad", "Retired", "Damp", "Rejected", "Redundant", "Forsaken", "" ];
+var adjectives = ["A Terrible", "A Haunted", "A Forgotten","A Stupified", "An Abandoned", "A Tortured", "An Inclement", "An Apologetic", "A Riddled", "An Unfortunate", "A Neglected", "A Forgotten",
+ "A Sordid", "A Mistaken", "A Glutonous", "A Bedraggled", "A Stricken", "A Confused", "A Beaten", "A Sad", "A Retired", "A Damp", "A Rejected", "A Redundant", "A Forsaken"];
 
 var nouns = ["Fish", "Coalmine", "Paddock", "Fishwife", "Cavern", "Railway", "Terrrapin", "Nozzle", "Shoe", "Phonebook", 'Postbox', "Column", "Dustbin", "Haddock", "Pudding",
 "Gibbon", "Heron", "Pier", "Social Club", "Landmine", "Starfish", "Crater", "Beaver", "Ferret", "Horse", "Lantern"];
@@ -7,12 +7,12 @@ var nouns = ["Fish", "Coalmine", "Paddock", "Fishwife", "Cavern", "Railway", "Te
 var name = prompt("What is your name?");
 
 function withYourFace(name){
-	alert(name+ " with your face like a "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
-	alert(name+" with your face like a "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
-	alert(name+" with your face like a "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
-	alert(name+ " with your face like a "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
-	alert(name+" with your face like a "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
-	alert(name+" with your face like a "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
+	alert(name+ " with your face like "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
+	alert(name+" with your face like "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
+	alert(name+" with your face like "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
+	alert(name+ " with your face like "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
+	alert(name+" with your face like "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
+	alert(name+" with your face like "+ adjectives[Math.floor(Math.random() * 24)]+" "+nouns[Math.floor(Math.random() * 26)]+".");
 }
 
 withYourFace(name)
